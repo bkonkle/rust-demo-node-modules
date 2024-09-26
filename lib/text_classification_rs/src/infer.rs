f436a9d4a7b7701e615c5beb26f045deadfd538f
Brandon Konkle <brandon@konkle.us>
2024-09-26 15:07:21 -0600

    Move to Buffers to take advantage of zero-copy TypedArrays.

--- lib/text_classification_rs/src/infer.rs
+++ lib/text_classification_rs/src/infer.rs
@@ -51,8 +51,12 @@ impl Inference {
     pub fn infer(&self, input: Buffer) -> napi::Result<Buffer> {
         let input = from_utf8(&input).map_err(|e| napi::Error::from_reason(e.to_string()))?;
 
-        let output = self.model.predict(vec![input]);
+        let output = self
+            .model
+            .predict(vec![input])
+            .pop()
+            .ok_or_else(|| napi::Error::from_reason("No model output was received"))?;
 
-        Ok(output[0].text.clone().into())
+        Ok(output.text.into())
     }
 }
